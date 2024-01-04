8bec39fba984692c76ad846b4917a841bbb7a87c
Nikita Nikitin <nickitin_2010@mail.ru>
2024-01-04 10:07:53 +0300

    Error handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import main from './schemas/main.js'
 import render from './dom/render.js'
 
-(async ()=>{
-  await render(main);
+(async () => {
+  try {
+    await render(main);
+  } catch (err) {
+    console.error(`On render error: ${err}`)
+  }
 })()
